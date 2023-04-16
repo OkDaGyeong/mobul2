08170d42bdbccf11f96a2aece033833d66d85fd8
gesalburger111 <121841768+gesalburger111@users.noreply.github.com>
2023-04-16 22:21:07 +0900

    파일업로드추가

--- src/main/java/com/codehows/mobul/service/BoardsService.java
+++ src/main/java/com/codehows/mobul/service/BoardsService.java
@@ -1,13 +1,17 @@
 package com.codehows.mobul.service;
 
 
+import com.codehows.mobul.dto.BoardsFormDTO;
 import com.codehows.mobul.entity.Boards;
+import com.codehows.mobul.entity.BoardsFile;
+import com.codehows.mobul.repository.BoardsFileRepository;
 import com.codehows.mobul.repository.BoardsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 //import org.springframework.web.multipart.MultipartFile;
 
 import javax.transaction.Transactional;
@@ -22,6 +26,12 @@ import java.util.UUID;
 public class BoardsService {
     @Autowired
     private BoardsRepository boardsRepository;
+
+    @Autowired
+    private final BoardsFileService boardsFileService;
+
+    @Autowired
+    private final BoardsFileRepository boardsFileRepository;
 
     // 파일 입력
     public void write(Boards boards) {boardsRepository.save(boards);}
@@ -47,36 +57,21 @@ public class BoardsService {
     }
 
 
+    public Long saveBoard(BoardsFormDTO boardsFormDTO, List<MultipartFile> fileList) throws Exception{
+        // 게시글 등록
+        Boards boards = boardsFormDTO.createBoard();
+        boardsRepository.save(boards);
 
+        // 이미지 등록
+        for(int i=0; i<fileList.size(); i++){
+            BoardsFile boardsFile = new BoardsFile();
+            boardsFile.setFileBoardNum(boards);
 
+            boardsFileService.saveFile(boardsFile, fileList.get(i));
+        }
 
-    // 파일 입력 -- 수정 필요
-//    public void write(Boards boards, MultipartFile file) throws Exception{
-//        // 파일 저장 경로
-//        String projectPath = System.getProperty("user.dir")+"\\src\\main\\resources\\static\\files";
-//        // 랜덤으로 파일 이름 만들기
-//        UUID uuid = UUID.randomUUID();
-//        String fileName = uuid + "-" + file.getOriginalFilename();
-//
-//        // 파일 넣을 객체
-//        File saveFile = new File(projectPath, fileName);
-//        file.transferTo(saveFile);
-//
-//        boardsRepository.save(boards);
-//    }
+        return boards.getBoardId();
+    }
 
-
-
-
-//    private  final BoardsRepository boardsRepository;
-
-
-//    public Long saveBoards(BoardsFormDTO boardsFormDTO) throws Exception{
-//        // 게시글 등록
-//        Boards boards = boardsFormDTO.createBoards();
-//        boardsRepository.save(boards);
-//
-//        return boards.getBoardId();
-//    }
 
 }
